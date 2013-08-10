dbf65fbae1b345591306c2ebe2a03f0e24062302
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-08-10 19:38:11 +0700

    fix font awesome, constrain scrolling

--- static/js/view_scroll.js
+++ static/js/view_scroll.js
@@ -9,6 +9,7 @@ function bindTo(view) {
   view.element.on('MozMousePixelScroll', function(e) {
     var pixels = e.originalEvent.detail
     view.viewport.scroll -= pixels
+    if (view.viewport.scroll < 0) view.viewport.scroll = 0
     view.redraw()
   })
 }
