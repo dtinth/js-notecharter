b25bd451801f9bfd476c8dc598a708117586bd09
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-08-21 13:29:23 +0700

    don't generate random notes anymore, use modern html5 scroll

--- static/js/doc.js
+++ static/js/doc.js
@@ -9,21 +9,6 @@ define(function(require) {
     var dirty = desire('dirty')
     var level = new models.Level()
 
-    var channels = ['1', '2', '3', '4', '5', '6', '7']
-
-    function generateRandomNote(i) {
-      level.batch(function() {
-        _.take(_.shuffle(channels), _.random(1, 2)).forEach(function(channel) {
-          var event = models.createEvent({ channel: channel, row: i * 12 })
-          level.addEvent(event)
-        })
-      })
-    }
-
-    for (var i = 0; i < 1000; i ++) {
-      generateRandomNote(i)
-    }
-
     return {
       level: level,
       watch: dirty.watch(function() { return level.revision }),
--- static/js/view_scroll.js
+++ static/js/view_scroll.js
@@ -9,8 +9,10 @@ var view = desire('view')
 var viewport = desire('viewport')
 
 function bind() {
-  view.element.on('MozMousePixelScroll', function(e) {
-    var pixels = e.originalEvent.detail
+  view.element.on('wheel', function(e) {
+    console.log(e)
+    e.preventDefault()
+    var pixels = e.originalEvent.deltaY
     viewport.scroll -= pixels
     if (viewport.scroll < 0) viewport.scroll = 0
     view.redraw()
